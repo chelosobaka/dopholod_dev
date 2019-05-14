af54525b81ab900d913bf56c39e3a51d4ab995f6
chelosobaka <chelosobaka93@mail.ru>
2019-05-14 21:21:13 +0300

    create galery without mobile adapted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 
 //= require jquery3
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require turbolinks
 //=	require_self
 //= require_tree .
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,11 @@
 module StaticPagesHelper
   def photo_for_slider(photo, index)
     if photo.image.present?
-      image_tag("#{photo.image}", class: "d-block w-100", alt: "#{index+1} slide")
+      image_tag("#{photo.image}", class: "photo-our-work", alt: "#{index+1} slide")
     elsif photo.image_link.present?
-      image_tag("#{photo.image_link}", class: "d-block w-100", alt: "#{index+1} slide")
+      image_tag("#{photo.image_link}", class: "photo-our-work", alt: "#{index+1} slide")
     else
-      image_tag("no_photo.png", class: "d-block w-100", alt: "#{index+1} slide")
+      image_tag("no_photo.png", class: "photo-our-work", alt: "#{index+1} slide")
     end
   end
 end
