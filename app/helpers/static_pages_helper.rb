49c41a1e1eb8e1361383a878701605119772ed2b
chelosobaka <chelosobaka93@mail.ru>
2019-05-17 22:46:14 +0300

    gallery finish

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,11 @@
 module StaticPagesHelper
   def photo_for_slider(photo, index)
     if photo.image.present?
-      image_tag("#{photo.image}", class: "photo-our-work", alt: "#{index+1} slide")
+      image_tag("#{photo.image}", alt: "#{index+1} slide")
     elsif photo.image_link.present?
-      image_tag("#{photo.image_link}", class: "photo-our-work", alt: "#{index+1} slide")
+      image_tag("#{photo.image_link}", alt: "#{index+1} slide")
     else
-      image_tag("no_photo.png", class: "photo-our-work", alt: "#{index+1} slide")
+      image_tag("no_photo.png", alt: "#{index+1} slide")
     end
   end
 end
