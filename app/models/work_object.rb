ca108c9c1de57fe50195e377d130270b2391457c
chelosobaka <chelosobaka93@mail.ru>
2019-05-18 00:29:07 +0300

    add dependent: destroy to work object

--- app/models/work_object.rb
+++ app/models/work_object.rb
@@ -1,5 +1,5 @@
 class WorkObject < ApplicationRecord
-  has_many :work_photos
+  has_many :work_photos, dependent: :destroy
   accepts_nested_attributes_for :work_photos
 
   validates :title, presence: true
