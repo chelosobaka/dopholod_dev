858d457cc09275a0cb14d49eb6735cae3cc45466
chelosobaka <chelosobaka93@mail.ru>
2019-05-21 17:02:33 +0300

    chenge about and color of coursors

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DopholodDev
     config.load_defaults 5.2
     config.i18n.default_locale = :ru
     config.assets.initialize_on_precompile = false
+    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
