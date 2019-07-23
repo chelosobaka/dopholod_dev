c519ee9ddbab3b6ea2b33c28e1c8375e157ec4b7
chelosobaka <chelosobaka93@mail.ru>
2019-07-23 23:18:09 +0300

    cap ready setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ lock "~> 3.11.0"
 set :application, "dopholod"
 set :repo_url, "git@github.com:chelosobaka/dopholod_dev.git"
 
+
+set :deploy_to, '/home/deploy/www'
+
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
